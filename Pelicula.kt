189b29c72081b769f5ec6771e11942ca57615648
TonyArPe <tonyaragonp@gmail.com>
2023-10-06 12:16:17 +0200

    Version 1.2 Version con errores corregidos

--- Pelicula.kt
+++ Pelicula.kt
@@ -8,112 +8,120 @@ class Pelicula(val codigo: Int, val nombre: String, val anno: Int) : this(){
     }
 }
 
-fun main(){
- val mapeoPeliculas = mutableMapOf<Int, Pelicula>()
- var siguienteCodigo = 1
+fun main()class Pelicula(val codigo: Int, val nombre: String, val anno: Int) {
+    override fun toString(): String {
+        return "CODIGO: $codigo, NOMBRE: $nombre, AÑO: $anno"
+    }
+}
 
- while(true){
-    println("\nMENU:")
-    println("1. Insertar una nueva película")
-    println("2. Mostrar una película por su código")
-    println("3. Buscar una película por su código")
-    println("4. Borrar una película por su código")
-    println("5. Editar una película")
-    println("6. Mostrar listado de peliculas")
-    println("7. Salir")
-    print("Seleccione una opción: ")
+fun main() {
+    val mapeoPeliculas = mutableMapOf<Int, Pelicula>()
+    var siguienteCodigo = 1
 
-    val eleccion = readLine()?.toIntOrNull() ?: 0
+    while (true) {
+        println("\nMENU:")
+        println("1. Insertar una nueva película")
+        println("2. Mostrar una película por su código")
+        println("3. Buscar una película por su código")
+        println("4. Borrar una película por su código")
+        println("5. Editar una película")
+        println("6. Mostrar listado de películas")
+        println("7. Salir")
+        print("Seleccione una opción: ")
 
-    when(eleccion){
-        1 -> {
-            println("Inserte los datos de la película:")
+        val eleccion = readLine()?.toIntOrNull() ?: 0
+
+        when (elección) {
+            1 -> {
+                println("Inserte los datos de la película:")
                 print("Nombre: ")
                 val nombre = readLine() ?: ""
                 print("Codigo: ")
                 val codigo = readLine()?.toIntOrNull() ?: 0
                 print("Año: ")
-                val anno = readLine()?.toIntOrNull() ?: 0
+                val año = readLine()?.toIntOrNull() ?: 0
 
-                val pelicula = Pelicula(codigo, nombre, anno)
+                val pelicula = Pelicula(codigo, nombre, año)
                 mapeoPeliculas[siguienteCodigo] = pelicula
                 siguienteCodigo++
                 println("Película insertada con éxito.")
-        }
-
-        2-> {
-            print("Ingrese el código de la película: ")
-            val codigo = readLine()?.toIntOrNull() ?: 0
-
-            val pelicula = mapeoPeliculas[codigo]
-            if (pelicula != null) {
-                println("Película encontrada:")
-                println(pelicula)
-            } else {
-                println("Película no encontrada.")
-            }
-        }
-
-        3 -> {
-            print("Ingrese el código de la película a buscar: ")
-            val codigo = readLine()?.toIntOrNull() ?: 0
-
-            val pelicula = mapeoPeliculas[codigo]
-            if (pelicula != null) {
-                println("Película encontrada:")
-                println(pelicula)
-            } else {
-                println("Película no encontrada.")
-            }
-        }
-
-        4 -> {
-            print("Ingrese el código de la película a borrar: ")
-            val codigo = readLine()?.toIntOrNull() ?: 0
-
-            val pelicula = mapeoPeliculas.remove(codigo)
-            if (pelicula != null) {
-                println("Película eliminada:")
-                println(pelicula)
-            } else {
-                println("Película no encontrada.")
-            }
-        }
-
-        5 -> {
-            print("Ingrese el código de la película a actualizar: ")
-            val codigo = readLine()?.toIntOrNull() ?: 0
-
-            val pelicula = mapeoPeliculas[codigo]
-            if (pelicula != null) {
-                println("Editar película:")
-                print("Nuevo título (${Pelicula.nombre}): ")
-                val nuevoNombre = readLine() ?: Pelicula.nombre
-                print("Nuevo Año (${Pelicula.anno}): ")
-                val nuevoAnno = readLine() ?: Pelicula.anno
-
-                val peliculaActualizada = Pelicula(codigo, nuevoNombre, nuevoAnno)
-                mapeoPeliculas[codigo] = peliculaActualizada
-                println("Película actualizada con éxito.")
-            } else {
-                println("Película no encontrada.")
-            }
-        }
-        
-        6 -> {
-            println("Listado de películas:")
-            mapeoPeliculas.forEach { (codigo, pelicula) ->
-                println("$codigo: $pelicula")
             }
 
-        7 -> {
+            2 -> {
+                print("Ingrese el código de la película: ")
+                val codigo = readLine()?.toIntOrNull() ?: 0
+
+                val pelicula = mapeoPeliculas[codigo]
+                if (pelicula != null) {
+                    println("Película encontrada:")
+                    println(pelicula)
+                } else {
+                    println("Película no encontrada.")
+                }
+            }
+
+            3 -> {
+                print("Ingrese el código de la película a buscar: ")
+                val codigo = readLine()?.toIntOrNull() ?: 0
+
+                val pelicula = mapeoPeliculas[codigo]
+                if (pelicula != null) {
+                    println("Película encontrada:")
+                    println(pelicula)
+                } else {
+                    println("Película no encontrada.")
+                }
+            }
+
+            4 -> {
+                print("Ingrese el código de la película a borrar: ")
+                val codigo = readLine()?.toIntOrNull() ?: 0
+
+                val pelicula = mapeoPeliculas.remove(codigo)
+                if (pelicula != null) {
+                    println("Película eliminada:")
+                    println(pelicula)
+                } else {
+                    println("Película no encontrada.")
+                }
+            }
+
+            5 -> {
+                print("Ingrese el código de la película a actualizar: ")
+                val codigo = readLine()?.toIntOrNull() ?: 0
+
+                val pelicula = mapeoPeliculas[codigo]
+                if (pelicula != null) {
+                    println("Editar película:")
+                    print("Nuevo título (${pelicula.nombre}): ")
+                    val nuevoNombre = readLine() ?: pelicula.nombre
+                    print("Nuevo Año (${pelicula.anno}): ")
+                    val nuevoAnno = readLine()?.toIntOrNull() ?: pelicula.anno
+
+                    val peliculaActualizada = Pelicula(codigo, nuevoNombre, nuevoAnno)
+                    mapeoPeliculas[codigo] = peliculaActualizada
+                    println("Película actualizada con éxito.")
+                } else {
+                    println("Película no encontrada.")
+                }
+            }
+
+            6 -> {
+                println("Listado de películas:")
+                mapeoPeliculas.forEach { (codigo, pelicula) ->
+                    println("$codigo: $pelicula")
+                }
+            }
+
+            7 -> {
                 println("Saliendo del programa. ¡Hasta luego!")
                 return
-        }
-        else -> println("Opción no válida. Por favor, seleccione una opción válida.")
+            }
+
+            else -> println("Opción no válida. Por favor, seleccione una opción válida.")
         }
     }
 }
-}
+
 
 
